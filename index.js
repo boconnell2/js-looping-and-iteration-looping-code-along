3dc8795c6bd961481b117d099825ea7825c6f624
boconnell2 <bmoconnell15@gmail.com>
2020-12-09 16:03:10 -0500

    commit tree 67f3078f12a719a679d8ee3278347c33f8971a43

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts){
+//     for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts
+// }
+
+let array = ["Ada", "Brendan", "Ali"]
+let event = "birthday"
+
+function writeCards(arrArg, eventName) {
+    let output = []
+    for (let i = 0; i < array.length; i++) {
+        output.push(`Thank you, ${arrArg[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return output
+}
+
+function countDown(counter) {
+    while ( counter >= 0) {
+        console.log(counter);
+        counter--;
+    }
+}
